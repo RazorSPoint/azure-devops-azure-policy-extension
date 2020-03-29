29b1ac50d344d0e69d7e6a4b82cff137c7379f12
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2020-03-29 13:27:58 +0200

    changed path for task installation

--- tools/tfxTestInstallCommands.ps1
+++ tools/tfxTestInstallCommands.ps1
@@ -2,11 +2,11 @@
 tfx login -u https://dev.azure.com/razorspoint-test
 
 # upload the deploy azure policy task
-tfx build tasks upload --task-path ./src/AzurePolicy/AzurePolicyV1
-tfx build tasks upload --task-path ./src/AzurePolicy/AzurePolicyV2
+tfx build tasks upload --task-path ./dist/AzurePolicy/AzurePolicyV1
+tfx build tasks upload --task-path ./dist/AzurePolicy/AzurePolicyV2
 
 # upload the deploy azure initiative task
-tfx build tasks upload --task-path ./src/AzureInitiative/AzureInitiativeV1
+tfx build tasks upload --task-path ./dist/AzureInitiative/AzureInitiativeV1
 
 # delete the task
 tfx build tasks delete --task-id ef6e39d1-dbac-47e0-8c6b-f5735aaa4096
@@ -15,3 +15,6 @@ tfx build tasks delete --task-id 28825f8c-9d9c-4561-8d2a-429aa4ca7271
 
  #### for publishing manually to test tenant  
  tfx extension publish --publisher razorspoint --share-with razorspoint-trashdummy --output-path ./bin --rev-version --token #PatToken#
+
+ ## install extension
+ tfx extension install --vsix C:\Users\SebastianSchütze\Downloads\RazorSPoint.rp-build-release-azurepolicy-0.5.119.vsix
